87f7f2315bcc2fedbe54f38e21e781ac39ffd914
fjandrad <ing.andrade.francisco@gmail.com>
2016-03-31 17:45:42 +0200

    added minor part for saving the new computed data in dataset2 and saving the calibration matrices files

--- main2.m
+++ main2.m
@@ -126,10 +126,10 @@ end
 % end
 
 % Plot ftDataNoOffset and/vs estimatedFtData
-% for i=4:4
-%     %     for i=1:size(input.ftNames,1)
-%     FTplots(struct(input.ftNames{i},dataset.ftDataNoOffset.(input.ftNames{i}),strcat('estimated',input.ftNames{i}),dataset.estimatedFtData.(input.ftNames{i})),dataset.time);
-% end
+for i=4:4
+    %     for i=1:size(input.ftNames,1)
+    FTplots(struct(input.ftNames{i},dataset.ftDataNoOffset.(input.ftNames{i}),strcat('estimated',input.ftNames{i}),dataset.estimatedFtData.(input.ftNames{i})),dataset.time);
+end
 
 % Plot forces in 3D space
 % %with offset
@@ -139,7 +139,6 @@ end
 %     plot3_matrix(dataset.estimatedFtData.(input.ftNames{i})(:,1:3)); grid on;
 % end
 
-
 %without offset
 for i=4:4
     %     for i=1:size(input.ftNames,1)
@@ -147,75 +146,47 @@ for i=4:4
     plot3_matrix(dataset.estimatedFtData.(input.ftNames{i})(:,1:3)); grid on;
 end
 
-% ellipsoid
-
-%TODO:
-%
-% for i=4:4
-%     %     %     for i=1:size(input.ftNames,1)
-%     %     figure,plot3_matrix(dataset.ftDataNoOffset.(input.ftNames{i})(:,1:3));hold on;
-%
-%     %     plot3_matrix(dataset.estimatedFtData.(input.ftNames{i})(:,1:3)); grid on;
-%
-%     % ellipsoidfit(dataset.ftDataNoOffset.(input.ftNames{i})(:,1),dataset.ftDataNoOffset.(input.ftNames{i})(:,2),dataset.ftDataNoOffset.(input.ftNames{i})(:,3))
-%     % ellipsoidfit_leastsquares(dataset.estimatedFtData.(input.ftNames{i})(:,1),dataset.estimatedFtData.(input.ftNames{i})(:,2),dataset.estimatedFtData.(input.ftNames{i})(:,3))
-%     ft_raw=dataset.ftDataNoOffset.(input.ftNames{i})(:,1:3);
-%     acc=dataset.estimatedFtData.(input.ftNames{i})(:,1:3);
-%     ft_raw_no_mean = ft_raw-ones(size(ft_raw,1),1)*mean(ft_raw);
-%     ft_raw_mean = mean(ft_raw);
-%
-%     [U_raw,S_ft_raw,V_raw] = svd(ft_raw_no_mean,'econ');
-%     bar((S_ft_raw));
-%     ft_raw_projector = V_raw(:,1:3)';
-%     ft_raw_projected = (V_raw(:,1:3)'*ft_raw_no_mean')';
-%     title('Raw sensor data singular values')
-%
-%     normalize = @(x) (x-ones(size(x,1),1)*mean(x))./(ones(size(x,1),1)*std(x));
-%     normalize_isotropically = @(x) (x-ones(size(x,1),1)*mean(x))/mean(std(x));
-%
-%     % normalize data
-%     ft_raw_projected_norm = normalize(ft_raw_projected);
-%     %%
-%     %Plotting ellipsoid fitted in raw space
-%     fprintf(['Fitting ft ellipsoid\n']);
-%     [p_ft_norm,ft_proj_norm_refitted]   = ellipsoidfit_smart(ft_raw_projected_norm,acc);
-%     fprintf(['Fitting acc ellipsoid\n']);
-%     p_acc  = ellipsoidfit(acc(1:end,1),acc(1:end,2),acc(1:end,3));
-%
-%     figure
-%     plot_ellipsoid_im(p_ft_norm);
-%     plot3_matrix(ft_raw_projected_norm(1:end,:))
-%     axis equal
-%     title('Ellipsoid fitted in FT raw space');
-%
-% end
-
-
-%%Calibration matrix correction
+%% Calibration matrix correction
 % filtered ft data
 [filteredFtData,mask]=filterFtData(dataset.ftData);
 
 dataset2=applyMask(dataset,mask);
 filterd=applyMask(filteredFtData,mask);
 dataset2.filteredFtData=filterd;
-% for i=4:4
-%     %     for i=1:size(input.ftNames,1)
-%     FTplots(struct(input.ftNames{i},filterd.(input.ftNames{i}),strcat('estimated',input.ftNames{i}),dataset2.estimatedFtData.(input.ftNames{i})),dataset2.time);
-% end
+for i=4:4
+    %     for i=1:size(input.ftNames,1)
+    FTplots(struct(input.ftNames{i},filterd.(input.ftNames{i}),strcat('estimated',input.ftNames{i}),dataset2.estimatedFtData.(input.ftNames{i})),dataset2.time);
+end
+
 %getting raw datat
 [dataset2.rawData,cMat]=getRawData(dataset2.filteredFtData,input.calibMatPath,input.calibMatFileNames);
-[calibMatrices,offset]=estimateMatrices(dataset2.rawData,dataset2.estimatedFtData);
+[calibMatrices,offset,fullscale]=estimateMatrices(dataset2.rawData,dataset2.estimatedFtData);
 eC=cMat.left_leg-calibMatrices.left_leg;
 
-for i=4:4
+for i=3:6
     for j=1:size(dataset2.rawData.(input.ftNames{i}),1)
-        reCalibData(j,:)=calibMatrices.(input.ftNames{i})*(dataset2.rawData.(input.ftNames{i})(j,:)'-offset.(input.ftNames{i})');
+        reCalibData.(input.ftNames{i})(j,:)=calibMatrices.(input.ftNames{i})*(dataset2.rawData.(input.ftNames{i})(j,:)'-offset.(input.ftNames{i})');
     end
 end
 
 for i=4:4
     %     for i=1:size(input.ftNames,1)
-    figure,plot3_matrix(reCalibData(:,1:3));hold on;
+%       figure,plot3_matrix(reCalibData.(input.ftNames{i})(:,1:3));hold on;
+%     figure,plot3_matrix(dataset.estimatedFtData.(input.ftNames{i})(:,1:3)); grid on;
+    figure,plot3_matrix(reCalibData.(input.ftNames{i})(:,1:3));hold on;
     plot3_matrix(dataset.estimatedFtData.(input.ftNames{i})(:,1:3)); grid on;
 end
 
+dataset2.calibMatrices=calibMatrices;
+dataset2.oofset=offset;
+dataset2.fullscale=fullscale;
+ %% Save the workspace
+    %     %save meaninful data, estimated data, meaninful data no offset
+    save(strcat('data/',experimentName,'/dataset2.mat'),'dataset2')
+  
+ %%write calibration matrices file
+for i=3:6
+    
+      filename=strcat('data/',experimentName,'/calibrationMatrices/',input.calibMatFileNames{i});
+    writeCalibMat(calibMatrices.(input.ftNames{i}), fullscale.(input.ftNames{i}), filename)
+end
