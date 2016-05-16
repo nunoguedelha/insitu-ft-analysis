8af7fbf55251b9ca339b1013b3652edea43eb5e6
fjandrad <fjandrad@users.noreply.github.com>
2016-05-16 14:28:48 +0200

    Try to estimate the calibration matrix and the offset.
    Researching literature and least square regularization methods I saw that using and augmented vector to include the offset in the calculations on methods with regularization may give solutions with priority to cross the center, (not estimating well the bias). So instead using the difference between the mean and the actualv value for the reference and the raw data allows to obtain the calibration matrix without bias and then offset can be estimated.

---
+++ utils/estimateMatriceS.m
@@ -0,0 +1,15 @@
+
+function  [calibMatrices,offset,fullscale]=estimateMatriceS(rawData,estimatedFtData,cMat,lambda)
+ftNames=fieldnames(estimatedFtData);
+n=3; %n=3 usually start from 3rd, start from first
+%add offset removal here? estimateOffsetusingInsitu(rawData(:,1:3), estimatedFtData(:.1:3))
+for i=n:size(ftNames,1)
+    meanFt=mean(rawData.(ftNames{i}));
+     meanEst=mean(estimatedFtData.(ftNames{i}));
+    rawNoMean=rawData.(ftNames{i})-repmat(meanFt,size(rawData.(ftNames{i}),1),1);
+    estNoMean=estimatedFtData.(ftNames{i})-repmat(meanEst,size(estimatedFtData.(ftNames{i}),1),1);
+%[calibMatrices.(ftNames{i}),fullscale.(ftNames{i})]=estimateCalibMatrix(rawNoMean,estNoMean.(ftNames{i}));
+[calibMatrices.(ftNames{i}),fullscale.(ftNames{i})]=estimateCalibMatrixWithReg(rawNoMean,estNoMean.(ftNames{i}),cMat.(ftNames{i}),lambda);
+
+offset.(ftNames{i})=estNoMean-calibMatrices.(ftNames{i})*rawNoMean;
+end
