3324150609316b382103d545b70d03200a3b55ad
fjandrad <ing.andrade.francisco@gmail.com>
2016-05-17 15:00:14 +0200

    Deleting datasets not found in gitLab repository, this can avoid having troubles downloading the functions in windows

--- data/dumperLeftLeg/params.m
+++
@@ -1,5 +0,0 @@
-%Parameter script
-contactInfo=0; % 1 if is on the right , 0 if is on the left
-relevant=1; %if relevantData file exists
-rData=[66.5,117]; %initial and final time of relevant data
-contactFrameName='l_sole'; %name of the frame which is in contact
--- data/dumperRightLegNoIMU/params.m
+++
@@ -1,5 +0,0 @@
-%Parameter script
-contactInfo=1; % 1 if is on the right , 0 if is on the left
-relevant=1; %if the info of the meaningful interval is known
-rData=[14.34,68.04]; %initial and final time of relevant data
-contactFrameName='r_sole'; %name of the frame which is in contact
