419010059fb521fb00f210254ebc536911efdfd1
fjandrad <ing.andrade.francisco@gmail.com>
2016-07-13 18:39:08 +0200

    Separation of generation of raw data and workbench calibration matrix loading.

--- utils/getRawData.m
+++ utils/getRawData.m
@@ -4,30 +4,10 @@ if (size(serialNumbers,1)~=size(ftNames,1))
     disp('error number of serial numbers does not match number of sensors')
     rawData=0;
 else
-    
     for i=1:size(serialNumbers,1)
-        
-        if (exist(strcat(pathFile,'matrix_',serialNumbers{i},'.txt'),'file')==2)
-            calibMat=readCalibMat(strcat(pathFile,'matrix_',serialNumbers{i},'.txt'));
-            calibMatrices.(ftNames{i})=calibMat;
+           calibMatrices.(ftNames{i})=getWorkbenchCalibMat(pathFile,serialNumbers{i});
             for j=1:size(ftData.(ftNames{i}))
-                rawData.(ftNames{i})(j,:)=calibMat\ftData.(ftNames{i})(j,:)';
+                rawData.(ftNames{i})(j,:)=calibMatrices.(ftNames{i})\ftData.(ftNames{i})(j,:)';
             end
-        else
-            disp(strcat({'getRawData: Calibration Matrix '},serialNumbers{i},{' not found in the specified folder. Trying in default folders'}))
-            if (exist(strcat('external/ftSensCalib/software/sensAquisitionArchive/',serialNumbers{i},'/','matrix_',serialNumbers{i},'.txt'),'file')==2)
-                calibMat=readCalibMat(strcat('external/ftSensCalib/software/sensAquisitionArchive/',serialNumbers{i},'/','matrix_',serialNumbers{i},'.txt'));
-                calibMatrices.(ftNames{i})=calibMat;
-                for j=1:size(ftData.(ftNames{i}))
-                    rawData.(ftNames{i})(j,:)=calibMat\ftData.(ftNames{i})(j,:)';
-                end
-            else
-                 
-            
-            disp(strcat({'getRawData: Calibration Matrix '},serialNumbers{i},{' not found in the defult folder.'}))
-            
-            rawData=0;
-            end
-        end
     end
 end
---
+++ utils/getWorkbenchCalibMat.m
@@ -0,0 +1,11 @@
+function [calibMatrix]=getWorkbenchCalibMat(pathFile,serialNumber)
+    if (exist(strcat(pathFile,'matrix_',serialNumber,'.txt'),'file')==2)
+        calibMatrix=readCalibMat(strcat(pathFile,'matrix_',serialNumber,'.txt'));
+    else
+        disp(strcat({'getRawData: Calibration Matrix '},serialNumber,{' not found in the specified folder. Trying in default folders'}))
+        if (exist(strcat('external/ftSensCalib/software/sensAquisitionArchive/',serialNumber,'/','matrix_',serialNumber,'.txt'),'file')==2)
+            calibMatrix=readCalibMat(strcat('external/ftSensCalib/software/sensAquisitionArchive/',serialNumber,'/','matrix_',serialNumber,'.txt'));
+        else
+            disp(strcat({'getRawData: Calibration Matrix '},serialNumber,{' not found in the defult folder.'}))
+        end
+    end
