9ac733f05342e606d158467896399075f2c537a6
fjandrad <ing.andrade.francisco@gmail.com>
2017-02-02 17:18:47 +0100

    correct use of varargin to exploit full potential of plot3 capabilities when using a matrix

--- utils/plot3_matrix.m
+++ utils/plot3_matrix.m
@@ -1,4 +1,4 @@
-function [ output_args ] = plot3_matrix( input_args )
+function [ output_args ] = plot3_matrix( input_args,varargin )
 %PLOT3_MATRIX version of plot3 that takes an input a single matrix 
 if( size(input_args,2) == 3) 
     x = input_args(:,1);
@@ -10,7 +10,10 @@ if( size(input_args,1) == 3)
     y = input_args(2,:);
     z = input_args(3,:);
 end
-output_args = plot3(x,y,z,'.');
-
+if(isempty(varargin))
+    output_args = plot3(x,y,z,'.');
+else
+output_args = plot3(x,y,z,varargin{:});
+end
 end
 
