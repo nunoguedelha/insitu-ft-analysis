cc33bbb8b3ac4a9ec482d24649892a73fc77064d
fjandrad <ing.andrade.francisco@gmail.com>
2017-03-15 11:43:02 +0100

    Fixed a bug and now it can manage reading over 10 files

--- utils/readMultiDataConnections.m
+++ utils/readMultiDataConnections.m
@@ -1,7 +1,18 @@
 function [data, time] = readMultiDataConnections(s,n)
 
-for i=1:n
+[data.data0,time.data0]=readDataDumper(s);
+
+for i=1:min(n,9)
     st=strrep(s, '/data.log', strcat('_0000',num2str(i),'/data.log'));
-    name=strcat('test',num2str(i));
-[data.(name),time.(name)]=datareadDataDumper(st);
+    name=strcat('data',num2str(i));
+[data.(name),time.(name)]=readDataDumper(st);
+end
+
+if(n>9 && n<100)
+    for i=10:n
+        st=strrep(s, '/data.log', strcat('_000',num2str(i),'/data.log'));
+        name=strcat('data',num2str(i));
+        [data.(name),time.(name)]=readDataDumper(st);
+    end
+    
 end
