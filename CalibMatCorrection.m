830d8f235e8408256c712312eb54af3ea262280e
Francisco <ing.andrade.francisco@gmail.com>
2017-12-08 14:03:37 +0100

    Firmware ignores full scale information so default full scale is used

--- CalibMatCorrection.m
+++ CalibMatCorrection.m
@@ -1,16 +1,11 @@
 %% Calibration matrix correction script
 % estimate new calibration matrices
 % assuming is run at the end of main, or after main
-%script options
 
-saveMat=true;
-usingInsitu=true;
-plot=true;
-onlyWSpace=true;
 %using insitu
 % NOTE: only use when position of center of mass is constant
 %TODO: procedure for choosing when to use insitu or not required
-if(usingInsitu)
+if(calibOptions.usingInsitu)
          % [calibMatrices,offset,fullscale]=estimateMatrices(dataset.rawData,dataset.estimatedFtData,sensorsToAnalize);
  [calibMatrices,offset,fullscale]= estimateMatricesWthReg(dataset.rawData,dataset.estimatedFtData,sensorsToAnalize, dataset.cMat,lambda);
  
@@ -51,33 +46,36 @@ reCabData.fullscale=fullscale;
 
 %% write calibration matrices file
 
-if(saveMat)
+if(calibOptions.saveMat)
     names=fieldnames(dataset.ftData);
      for ftIdx =1:length(sensorsToAnalize)
         ft = sensorsToAnalize{ftIdx};
         i=find(strcmp(ft, names));
         filename=strcat('data/',experimentName,'/calibrationMatrices/',dataset.calibMatFileNames{i},lambdaName);
-        writeCalibMat(calibMatrices.(ft), fullscale.(ft), filename)
+        defualtScale=[32767,32767,32767,32767,32767,32767]; %% this might work only for strain2 for reasons unknown
+        writeCalibMat(firmwareMat, defualtScale, filename)
     end
 end
 %% generate wrenches with new calibration matrix
-if(usingInsitu)
+if(calibOptions.usingInsitu)
+    
+    
     
     for ftIdx =1:length(sensorsToAnalize)
         ft = sensorsToAnalize{ftIdx};
         for j=1:size(dataset.rawData.(ft),1)
-            reCalibData.(ft)(j,:)=calibMatrices.(ft)*(dataset.rawData.(ft)(j,:)'-offset.(ft)');
+            reCalibData.(ft)(j,:)=calibMatrices.(ft)*(dataset.rawData.(ft)(j,:)'-offset.(ft)');            
             offsetInsitu.(ft)=calibMatrices.(ft)*offset.(ft)';
         end
     end
     reCabData.offsetInsitu=offsetInsitu;
 else
- for ftIdx =1:length(sensorsToAnalize)
+    for ftIdx =1:length(sensorsToAnalize)
         ft = sensorsToAnalize{ftIdx};
-    for j=1:size(dataset.rawData.(ft),1)
-        reCalibData.(ft)(j,:)=calibMatrices.(ft)*(dataset.rawData.(ft)(j,:)')+offsetC.(ft);
+        for j=1:size(dataset.rawData.(ft),1)
+            reCalibData.(ft)(j,:)=calibMatrices.(ft)*(dataset.rawData.(ft)(j,:)')+offsetC.(ft);
+        end
     end
- end
 end
 reCabData.reCalibData=reCalibData;
 reCabData.calibMatFileNames=dataset.calibMatFileNames;
@@ -85,20 +83,19 @@ reCabData.calibMatFileNames=dataset.calibMatFileNames;
     %     %save recalibrated matrices, offsets, new wrenches, sensor serial
     %     numbers
     if(scriptOptions.saveData)
-        if (usingInsitu)
+        if (calibOptions.usingInsitu)
              save(strcat('data/',experimentName,'/reCabDataInsitu.mat'),'reCabData')
         else
     save(strcat('data/',experimentName,'/reCabData.mat'),'reCabData')
         end
     end
-
-if(plot)
-    
+%% Plotting section
+if(calibOptions.plot)    
     %% plot 3D graph
-    if (onlyWSpace)
+    if (calibOptions.onlyWSpace)
         for ftIdx =1:length(sensorsToAnalize)
             ft = sensorsToAnalize{ftIdx};
-            if(usingInsitu)
+            if(calibOptions.usingInsitu)
                 filteredOffset.(ft)=(dataset.cMat.(ft)*offset.(ft)')';               
             else
                 filteredOffset.(ft)=offsetC.(ft)';
@@ -107,11 +104,17 @@ if(plot)
             filteredNoOffset.(ft)=dataset.filteredFtData.(ft) -repmat(filteredOffset.(ft),size(dataset.filteredFtData.(ft),1),1);
             
             figure,
-            plot3_matrix(filteredNoOffset.(ft)(:,1:3)); grid on;hold on;
+            if(~scriptOptions.firstTime)
+                plot3_matrix(filteredNoOffset.(ft)(:,1:3)); grid on;hold on;
+            end
             plot3_matrix(dataset.estimatedFtData.(ft)(:,1:3)); grid on;hold on;
             plot3_matrix(reCalibData.(ft)(:,1:3));
             
-            legend('measuredDataNoOffset','estimatedData','reCalibratedData','Location','west');
+            if(~scriptOptions.firstTime)
+                legend('measuredDataNoOffset','estimatedData','reCalibratedData','Location','west');
+            else
+                legend('estimatedData','reCalibratedData','Location','west');
+            end
             title(strcat({'Wrench space '},escapeUnderscores(ft),{' '},escapeUnderscores(lambdaName)));
             xlabel('F_{x}');
             ylabel('F_{y}');
