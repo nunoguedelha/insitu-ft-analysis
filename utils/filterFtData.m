5599fff0bbe00134f43685fa6eaa7433e4b169e0
Francisco <ing.andrade.francisco@gmail.com>
2017-12-09 17:54:55 +0100

    change to avoid losing measurments giving zero

--- utils/filterFtData.m
+++ utils/filterFtData.m
@@ -9,16 +9,14 @@ function [filteredFtData,mask]=filterFtData(ftData)
 ftNames=fieldnames(ftData);
 N=2;
 F=101;
-%premask= filteredFtData.(ftNames{1})(:,1)==0;
+mask= false(size(ftData.(ftNames{1}),1),1);
 for i=1:size(ftNames,1)
     filteredFtData.(ftNames{i})=zeros(size(ftData.(ftNames{i})));
     for channel=1:size(ftData.(ftNames{i}),2)
         y = ftData.(ftNames{i})(:,channel);
         nrOfSamples = length(ftData.(ftNames{i}));
         [b,g] = sgolay(N,F);
-        HalfWin  = ((F+1)/2) -1;
-        %premask=filteredFtData.(ftNames{i})(:,channel)==0 ||; TODO
-        %consider the ceros already present not to be erased by mask
+        HalfWin  = ((F+1)/2) -1;        
         for n = (F+1)/2:nrOfSamples-(F+1)/2
             % Zeroth derivative (smoothing only)
             filteredFtData.(ftNames{i})(n,channel) = dot(g(:,1),y(n - HalfWin:n + HalfWin));
@@ -27,4 +25,4 @@ for i=1:size(ftNames,1)
     end
 end
 
-mask= filteredFtData.(ftNames{i})(:,1)~=0;
+mask((F+1)/2:nrOfSamples-(F+1)/2)=1;
