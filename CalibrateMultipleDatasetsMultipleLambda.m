01e4986740a447d0b1b9a4210fc9ce2a96059d69
Francisco <ing.andrade.francisco@gmail.com>
2017-12-10 05:43:29 +0100

    Changed names to be more clear of content and erased unnecesary scripts

--- CalibrateMultipleDatasetsMultipleLambda.m
+++ CalibrateMultipleDatasetsMultipleLambda.m
@@ -1,16 +1,9 @@
 %add required folders for use of functions
 addpath external/quadfit
 addpath utils
-% obtain data from all listed experiments
 
+% obtain data from all listed experiments
 experimentNames={
-%     'icub-insitu-ft-analysis-big-datasets/2016_06_08/yoga';% Name of the experiment;
-% 'icub-insitu-ft-analysis-big-datasets/2016_06_17/normal';% Name of the experiment;
-% 'icub-insitu-ft-analysis-big-datasets/2016_06_17/fast';% Name of the experiment;
-% 'icub-insitu-ft-analysis-big-datasets/2016_07_04/normal';% Name of the experiment;
-% 'icub-insitu-ft-analysis-big-datasets/2016_07_04/fast';% Name of the experiment;
-%   'icub-insitu-ft-analysis-big-datasets/2016_07_05/gridMin30';% Name of the experiment;
-% 'icub-insitu-ft-analysis-big-datasets/2016_07_05/gridMin45'% Name of the experiment;
  'green-iCub-Insitu-Datasets/2017_08_29_2';
  'green-iCub-Insitu-Datasets/2017_08_29_3';% Name of the experiment;
     };
@@ -42,33 +35,6 @@ for namingIndex=1:length(lambdas)
     end
 end
 lambdasNames=lambdasNames'
-
-% lambdas=[0;
-%     0.5;
-%     100;
-%     200;
-%     500;
-%     1000;
-%     3000;
-%     5000;
-%     10000;
-%     15000;
-%     30000;
-%     50000;
-%     100000];
-% lambdasNames={'';
-%     '_l_5';
-%     '_l100';
-%     '_l200';
-%     '_l500';
-%     '_l1000';
-%     '_l3000';
-%     '_l5000';
-%     '_l10000';
-%     '_l15000';
-%     '_l30000';
-%     '_l50000';
-%     '_l100000';};
 
 calculate=true;
 %%
