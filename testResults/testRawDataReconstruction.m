1184a04c790c2d7c7875299e3d3786769aff989d
Francisco <ing.andrade.francisco@gmail.com>
2018-04-11 11:10:29 +0200

    Nice script for comparing raw data and a calculated forces between datasets

--- testResults/testRawDataReconstruction.m
+++ testResults/testRawDataReconstruction.m
@@ -5,18 +5,17 @@
 % clear all;
 addpath ../utils
 addpath ../external/quadfit
-serialNumber='SN163';
-sensorsToAnalize = {'left_leg'};
+serialNumber='SN282';
+sensorsToAnalize = {'right_leg'};
 
 e=1; %one for doing comparison on experiment1 data, 2 for experiment
 [workbench,fullscale] = readCalibMat(strcat('../data/sensorCalibMatrices/matrix_',serialNumber,'.txt'));
 
 % experiment 1
 
-toCompare='/green-iCub-Insitu-Datasets/2017_10_31_4';
+toCompare='/green-iCub-Insitu-Datasets/2018_04_09_Grid';
 scriptOptions = {};
 scriptOptions.forceCalculation=false;%false;
-scriptOptions.printPlots=true;%true
 scriptOptions.raw=false;
 scriptOptions.saveData=true;
 scriptOptions.testDir=true;% to calculate the raw data, for recalibration always true
@@ -29,9 +28,9 @@ scriptOptions.matFileName='rawDataset';
 %[dataset,~,~]=read_estimate_experimentData(experimentName,scriptOptions);
 [dataset,~,~]=readExperiment (toCompare,scriptOptions);
 
-compareExp='icub-insitu-ft-analysis-big-datasets/2017_12_20_Green_iCub_leftLegFoot/validatePoleGridLeftLeg'; % Name of the experiment;
+compareExp='/green-iCub-Insitu-Datasets/2018_04_09_Grid_Raw'; % Name of the experiment;
 
-scriptOptions.matFileName='ftDataset';
+scriptOptions.matFileName='rawDataset';
 % load dataset to use as reference
 [refDataset,~,~]=readExperiment (compareExp,scriptOptions);
 
@@ -43,32 +42,68 @@ ftNames=fieldnames(refDataset.ftData);
 %     ft = sensorsToAnalize{ftIdx};
 %     offset.(ft)=estimateOffsetUsingInSitu(e1.dataset.rawData.(ft), e1.dataset.estimatedFtData.(ft)(:,1:3));
 % end
-%recalibrate with matrix 1 experiment1
 
+%% compare raw data
 for ftIdx =1:length(sensorsToAnalize)
     ft = sensorsToAnalize{ftIdx};
     for j=1:size(dataset.ftData.(ft),1)
         rawCalculated.(ft)(j,:)=(workbench)\(dataset.ftData.(ft)(j,:)');%-offset.(ft)([4,5,6,1,2,3])');
+         rawCalculatedFiltered.(ft)(j,:)=(workbench)\(dataset.filteredFtData.(ft)(j,:)');%-offset.(ft)([4,5,6,1,2,3])');
     end
 end
 
-%remove offset
-% ftDataNoOffset1=removeOffset(reCalibData1,e1.dataset.estimatedFtData.(ftNames{i}));
 
+reference.(ft)=refDataset.ftData.(ft);
 
+FTplots(rawCalculated,dataset.time,reference,refDataset.time,'byChannel','raw');
+FTplots(rawCalculated,dataset.time,reference,refDataset.time,'raw');
 
-FTplots(rawCalculated,dataset.time,'raw');
+ft = 'right_leg';
+    names={'calculatedRawData','referenceRawData'};
+force3DPlots(names,ft,rawCalculated.(ft), refDataset.ftData.(ft)) ;
 
+referenceFiltered.(ft)=refDataset.filteredFtData.(ft);
 
-FTplots(refDataset.ftData,refDataset.time,'raw');
+FTplots(rawCalculatedFiltered,dataset.time,referenceFiltered,refDataset.time,'byChannel','raw');
+FTplots(rawCalculatedFiltered,dataset.time,referenceFiltered,refDataset.time,'raw');
 
-figure,plot3_matrix(rawCalculated.(ft)(:,1:3));hold on;
-plot3_matrix(refDataset.rawData.(ft)(:,1:3)); grid on;  hold on;
+ft = 'right_leg';
+    names={'calculatedRawData','referenceRawData'};
+force3DPlots(names,ft,rawCalculatedFiltered.(ft), referenceFiltered.(ft)) ;
 
+l=min([length(rawCalculatedFiltered.(ft));length(referenceFiltered.(ft))]);
+rawDiff=rawCalculatedFiltered.(ft)(1:l,:)-referenceFiltered.(ft)(1:l,:);
+rawMeanDiff=mean(abs(rawDiff));
+rawMaxDiff=max(abs(rawDiff));
 
-legend('normal','reference','Location','west');
-title('Force 3D space');
-xlabel('F_{x}');
-ylabel('F_{y}');
-zlabel('F_{z}');
+%% compare ft data
+for ftIdx =1:length(sensorsToAnalize)
+    ft = sensorsToAnalize{ftIdx};
+    for j=1:size(refDataset.ftData.(ft),1)
+        ftCalculated.(ft)(j,:)=(workbench)*(refDataset.ftData.(ft)(j,:)');%-offset.(ft)([4,5,6,1,2,3])');
+        ftCalculatedFiltered.(ft)(j,:)=(workbench)*(refDataset.filteredFtData.(ft)(j,:)');%-offset.(ft)([4,5,6,1,2,3])');
+    end
+end
 
+ftReference.(ft)=dataset.ftData.(ft);
+
+FTplots(ftCalculated,refDataset.time,ftReference,dataset.time,'bychannel');
+FTplots(ftCalculated,refDataset.time,ftReference,dataset.time);
+
+ft = 'right_leg';
+    names={'ftCalculated','referenceFtData'};
+force3DPlots(names,ft,ftCalculated.(ft), dataset.ftData.(ft)) 
+
+ftReferenceFiltered.(ft)=dataset.filteredFtData.(ft);
+
+FTplots(ftCalculatedFiltered,refDataset.time,ftReferenceFiltered,dataset.time,'bychannel');
+FTplots(ftCalculatedFiltered,refDataset.time,ftReferenceFiltered,dataset.time);
+
+ft = 'right_leg';
+    names={'ftCalculated','referenceFtData'};
+force3DPlots(names,ft,ftCalculatedFiltered.(ft), ftReferenceFiltered.(ft)) 
+
+l=min([length(ftCalculatedFiltered.(ft));length(ftReferenceFiltered.(ft))]);
+diff=ftCalculatedFiltered.(ft)(1:l,:)-ftReferenceFiltered.(ft)(1:l,:);
+meanDiff=mean(abs(diff));
+maxDiff=max(abs(diff));
